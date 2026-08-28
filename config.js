6a1dc99305bb0537a22b4ceb7e6b718dff9dac7f
Jhonatan <lokonike@hotmail.com.br>
2026-08-27 21:23:52 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js — NÃO suba esse arquivo pro git (.gitignore)
+const CONFIG = {
+  EMAILJS_PUBLIC_KEY:  "SilTIK82Gx8Xppngy",
+  EMAILJS_SERVICE_ID:  "service_52vx18c",
+  EMAILJS_TEMPLATE_ID: "template_42hdfeo",
+  DESTINATION_EMAIL:   "jhonyskylines@gmail.com"
+  };
